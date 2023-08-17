bf8048b3176674d0700aec6b77050a3730d22cc1
dpera005xamk <dpera005@edu.xamk.fi>
2023-08-17 15:53:09 +0300

    updt demo3

--- demo03/index.ts
+++ demo03/index.ts
@@ -3,7 +3,7 @@ import { PrismaClient } from "@prisma/client";
 import path from "path";
 import multer from "multer"; // kun käytetään multiform part form tapaa npm install npm install multer
                             // npm install --save-dev @types/multer
-import fs from "fs/promises";
+import fs from "fs/promises"; // file systemin promise versio, että toimii async
 
 // Jatka: Video 20:38!
 
@@ -11,15 +11,16 @@ const app : express.Application = express();
 const prisma : PrismaClient = new PrismaClient();
 
 const uploadKasittelija : express.RequestHandler = multer({  // multerin ilmentymä
-    dest : path.resolve(__dirname, "tmp"), // mihin menee
+    dest : path.resolve(__dirname, "tmp"), // mihin menee, eli tmp kansioon, aina hyvä käyttää tempiä eka, ei suoraan tuotantoon
     limits : {
-        fileSize : (1024 * 500)
+        fileSize : (1024 * 500) //500 kilotavua, eli tuo jälkimmäinen oikeastaan vastaa kilotavujen määrää
     },
-    fileFilter : (req, file, callback) => {
-
-        if (["jpg", "jpeg"].includes(file.mimetype.split("/")[1])) {
-
-            callback(null, true);
+    fileFilter : (req, file, callback) => { // callback: functio nimettynä, jota kutsutaan myöhemmin, yleensä näin nimetty callbackiksi
+        // console.log(file.mimetype.split("/"[1])); pitäisi tulostaa esim. "png"
+        if (["jpg", "jpeg"].includes(file.mimetype.split("/")[1])) { // tässä tiedoston tyypit, hyvä jo tässä vaiheessa, ennen tallennusta.
+                                        // mime tyyppi on tuo mikä tyyppi, esim. image/png
+                                        // pilkkoo / kohdasta, eli otetaan 1, koska se on solu 1, eli tulostuu vaan "png"
+            callback(null, true); // true tarkottaa, että hyväksyy
 
         } else {
 
@@ -30,7 +31,7 @@ const uploadKasittelija : express.RequestHandler = multer({  // multerin ilmenty
         
 
     }
-}).single("tiedosto");
+}).single("tiedosto"); // yksi tiedosto
 
 const portti : number = Number(process.env.PORT) || 3003;
 
@@ -45,20 +46,26 @@ app.set("view engine", "ejs");
 // css voit tehdä vaikka sit styles.css ja sinne sisällöksi, mitä olisi stylesissä
 app.use(express.static(path.resolve(__dirname, "public")));
 
+// näin taitaisi toimia myös:
+//app.post("/lisaa", uploadKasittelija.single("tiedosto"), async......PrismaClient.
+// tällein sen näkisi, eli siitä tulisi paljon tietoa:
+// res.send(JSON.stringify(req.file));
 app.post("/lisaa", async (req : express.Request, res : express.Response ) => {
 
     uploadKasittelija(req, res, async (err : any) => {
 
-        if (err instanceof multer.MulterError) {
+        if (err instanceof multer.MulterError) { // tämä triggeröityy, jos multerista tuleva erroria
+                    // mihin ejs tiedostoon, eli "lisaa" tässä tapauksessa.                                   tällä se bodyn teksti viedään sinne
             res.render("lisaa", { "virhe" : "Tiedosto on tiedostokooltaan liian suuri (> 500kt).", "teksti" : req.body.teksti });
-        } else if (err) {
+        } else if (err) { // joku muu error, kuin multer errori
             res.render("lisaa", { "virhe" : "Väärä tiedostomuoto. Käytä ainoastaan jpg-kuvia.", "teksti" : req.body.teksti });        
         } else {
 
-            if (req.file) {
+            if (req.file) { // jos req.file on olemassa, niin tapahtuu seuraavaa
 
                 let tiedostonimi : string = `${req.file.filename}.jpg`; 
         
+                // kopioi tmp:stä tuonne publiciin, mistä ne näytetään.
                 await fs.copyFile(path.resolve(__dirname, "tmp", String(req.file.filename)), path.resolve(__dirname, "public", "img", tiedostonimi))
         
                 await prisma.kuva.create({
@@ -82,7 +89,7 @@ app.post("/lisaa", async (req : express.Request, res : express.Response ) => {
 
 app.get("/lisaa", async (req : express.Request, res : express.Response ) => {
 
-    res.render("lisaa", {virhe : "", teksti : ""});
+    res.render("lisaa", {virhe : "", teksti : ""}); // pitää olla nämä virhe ja teksti, että voi käyttää niitä
 
 });
 
